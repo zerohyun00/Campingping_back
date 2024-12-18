f4af4203a2fdd242429001c1020abd6e842d51ea
[Node1] 김영현 <kyhh39@naver.com>
2024-12-18 16:12:22 +0900

    db 세팅 및 base-table

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Exclude } from 'class-transformer';
+import { BaseTable } from 'src/common/entities/base-table.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum Role {
+  admin,
+  user,
+}
+
+@Entity()
+export class User extends BaseTable {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  @Exclude({
+    toPlainOnly: true, // 응답을 할 때 적용
+  })
+  password: string;
+}
