9636a29e25d8c2b41437e6369c6e44acfe675394
[Node1] 백기준 <rlwns1580@naver.com>
2024-12-24 17:50:38 +0900

    1

--- src/review/dto/param-review.dto.ts
+++ src/review/dto/param-review.dto.ts
@@ -1,14 +1,16 @@
 import { Type } from "class-transformer";
 import { IsNumber, IsString } from "class-validator";
+import { numberValidationMessage } from "src/common/validation-message/number-validation.message";
+import { stringValidationMessage } from "src/common/validation-message/string-validation.message";
 
 export class ParamReview {
     @Type(() => Number)
-    @IsNumber()
+    @IsNumber({}, {message: numberValidationMessage})
     id: number;
 }
 
 export class FindReviewParam {
     @Type(() => String)
-    @IsString()
+    @IsString({message: stringValidationMessage})
     contentId: string;
 }
