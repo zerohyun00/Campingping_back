3910b61e410cce896e842db789faee756ef27f8c
[Node1] 김영현 <kyhh39@naver.com>
2024-12-29 23:27:59 +0900

    docs : 기본 swagger 세팅

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('캠핑핑')
+    .setDescription('캠핑핑 API 명세서')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('doc', app, document);
+
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
