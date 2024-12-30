d1d4b729a726edc94a50688a889e418fc2097146
[Node1] 김영현 <kyhh39@naver.com>
2024-12-30 22:29:39 +0900

    docs : prefix에 api 추가

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,50 +1,75 @@
-import { BadRequestException, Inject, Injectable, NotFoundException } from "@nestjs/common";
-import { CreateReviewDto } from "./dto/create-review.dto";
-import { ReviewRepository } from "./repository/review.repository";
-import { UserService } from "src/user/user.service";
-import { FindReviewParam, ParamReview } from "./dto/param-review.dto";
-import { updateReviewDto } from "./dto/update-review.dto";
-import { ReponseReviewDto } from "./dto/response-review.dto";
-import { CampingService } from "src/camping/camping.service";
-
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CampingService } from 'src/camping/camping.service';
+import { UserService } from 'src/user/user.service';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { FindReviewParam, ParamReview } from './dto/param-review.dto';
+import { ReponseReviewDto } from './dto/response-review.dto';
+import { updateReviewDto } from './dto/update-review.dto';
+import { ReviewRepository } from './repository/review.repository';
 
 @Injectable()
 export class ReviewService {
-    constructor(
-        private campingService: CampingService,
-        private reviewRepository: ReviewRepository,
-        private userService: UserService,
-    ){}
-    
-    async createReview(createReviewDto: CreateReviewDto, userId: string) {
-        const user = await this.userService.findOne(userId);
-        if(!user) throw new NotFoundException('로그인이 필요한 서비스 입니다.');
+  constructor(
+    private campingService: CampingService,
+    private reviewRepository: ReviewRepository,
+    private userService: UserService,
+  ) {}
 
-        const camping = await this.campingService.findOne(createReviewDto);
-        if(!camping) throw new NotFoundException('조회할 캠핑장이 없습니다.');
+  async createReview(createReviewDto: CreateReviewDto, userId: string) {
+    const user = await this.userService.findOne(userId);
+    if (!user) throw new NotFoundException('로그인이 필요한 서비스 입니다.');
 
-        const result = await this.reviewRepository.createReview(createReviewDto, user);
-        return result;
-    }
-    async getReview(findReviewParam: FindReviewParam){
-        const camping = await this.campingService.findOne(findReviewParam);
-        if(!camping) throw new NotFoundException('조회할 캠핑장이 없습니다.');
+    const camping = await this.campingService.findOne(createReviewDto);
+    if (!camping) throw new NotFoundException('조회할 캠핑장이 없습니다.');
 
-        const result = await this.reviewRepository.getReview(findReviewParam.contentId);
-        if(!result) throw new NotFoundException('리뷰가 존재하지않습니다.');
+    const result = await this.reviewRepository.createReview(
+      createReviewDto,
+      user,
+    );
+    return result;
+  }
+  async getReview(findReviewParam: FindReviewParam) {
+    const camping = await this.campingService.findOne(findReviewParam);
+    if (!camping) throw new NotFoundException('조회할 캠핑장이 없습니다.');
 
-        return ReponseReviewDto.allList(result);
-    }
-    async updateReview(paramReview: ParamReview, updateReviewDto: updateReviewDto, userId: string) {
-        const result = await this.reviewRepository.updateReview(paramReview.id, userId, updateReviewDto);
-        if (result.affected === 0) throw new BadRequestException('존재하지 않거나 해당 사용자가 작성한 리뷰가 아닙니다.');
-        
-        return;
-    }
-    async deleteReview(paramReview: ParamReview, userId: string) {
-        const result = await this.reviewRepository.deleteReview(paramReview.id, userId);
-        if (result.affected === 0) throw new BadRequestException('리뷰가 존재하지 않거나 삭제할 권한이 없습니다.');
-    
-        return;
-    }
+    const result = await this.reviewRepository.getReview(
+      findReviewParam.contentId,
+    );
+    if (!result) throw new NotFoundException('리뷰가 존재하지않습니다.');
+
+    return ReponseReviewDto.allList(result);
+  }
+  async updateReview(
+    paramReview: ParamReview,
+    updateReviewDto: updateReviewDto,
+    userId: string,
+  ) {
+    const result = await this.reviewRepository.updateReview(
+      paramReview.id,
+      userId,
+      updateReviewDto,
+    );
+    if (result.affected === 0)
+      throw new BadRequestException(
+        '존재하지 않거나 해당 사용자가 작성한 리뷰가 아닙니다.',
+      );
+
+    return;
+  }
+  async deleteReview(paramReview: ParamReview, userId: string) {
+    const result = await this.reviewRepository.deleteReview(
+      paramReview.id,
+      userId,
+    );
+    if (result.affected === 0)
+      throw new BadRequestException(
+        '리뷰가 존재하지 않거나 삭제할 권한이 없습니다.',
+      );
+
+    return;
+  }
 }
