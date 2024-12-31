3cb05c3e2cb74f38ba2e5eb50a613de6eba06ac6
[Node1] 백기준 <rlwns1580@naver.com>
2024-12-31 12:00:28 +0900

    설정 변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 
-@Controller('api')
+@Controller()
 export class AppController {
   constructor(private dataSource: DataSource) {}
-
-  @Get('doc')
-  getSwaggerDoc() {
-    return {
-      message: 'Swagger UI is available at /doc endpoint',
-    };
-  }
 }
--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://kdt-react-node-1-team03.elicecoding.com', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   SwaggerModule.setup('doc', app, document);
