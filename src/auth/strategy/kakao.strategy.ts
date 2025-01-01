a2bf5e25e11898e655d154a0c72a786a71336e0f
[Node1] 김영현 <kyhh39@naver.com>
2025-01-01 17:34:30 +0900

    test : vm에서 env전달 확인 테스트

--- src/auth/strategy/kakao.strategy.ts
+++ src/auth/strategy/kakao.strategy.ts
@@ -3,25 +3,24 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Profile, Strategy } from 'passport-kakao';
 
-@Injectable() // 이 데코레이터를 반드시 추가
+@Injectable() // 이 데코레이터를 추가해 NestJS 의존성 주입 활성화
 export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
   constructor(private readonly configService: ConfigService) {
-    const clientID = configService.get<string>('KAKAO_CLIENT_ID');
-    const clientSecret = configService.get<string>('KAKAO_CLIENT_SECRET');
-    const callbackURL = configService.get<string>('KAKAO_CALLBACK_URL');
-    console.log(clientID);
     super({
-      clientID,
-      clientSecret,
-      callbackURL,
+      clientID: configService.get<string>('KAKAO_CLIENT_ID'),
+      clientSecret: configService.get<string>('KAKAO_CLIENT_SECRET'),
+      callbackURL: configService.get<string>('KAKAO_CALLBACK_URL'),
     });
+    console.log(configService.get<string>('KAKAO_CLIENT_ID'));
   }
 
   async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    // 카카오 계정의 이메일 정보와 닉네임 가져오기
     const email = profile._json.kakao_account?.email;
     const nickname = profile.displayName;
     const kakaoId = profile.id;
 
+    // 이메일이 없는 경우, kakaoId를 사용하여 임시 이메일 생성
     const generatedEmail = email || `${kakaoId}@kakao.com`;
 
     console.log('accessToken: ', accessToken);
@@ -32,8 +31,8 @@ export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
     return {
       email: generatedEmail,
       nickname,
-      kakaoId,
-      type: 'KAKAO',
+      kakaoId, // 고유한 Kakao ID 추가
+      type: 'KAKAO', // 로그인 유형 추가
     };
   }
 }
