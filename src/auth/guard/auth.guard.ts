fe18163390f9ce51dcc6c4a024c42d7f603a0ec7
[Node1] 백기준 <rlwns1580@naver.com>
2025-01-23 14:10:01 +0900

    ref: 카카오 로그인 로직 수정

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class KakaoAuthGuard extends AuthGuard('kakao') {}
+export class KakaoAuthGuard extends AuthGuard('kakao') {
+handleRequest(err, user, info, context) {
+    if (err || !user) {
+        throw err || new UnauthorizedException('로그인 실패');
+    }
+    return user; // 리다이렉션 대신 사용자 정보 반환
+    }
+}
