8271051aa6e98372f263707d5fd6dc83671b799e
[Node1] 백기준 <rlwns1580@naver.com>
2025-01-27 21:33:05 +0900

    ref: 카카오로그인 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -127,7 +127,7 @@ export class AuthController {
 
     res.cookie('refreshToken', refreshToken, { httpOnly: true });
     res.cookie('accessToken', accessToken, { httpOnly: true });
-    res.redirect(`/sign-in?fromKaKao=true&email=${email}`);
+    res.redirect(`http://localhost:3000/sign-in?fromKaKao=true&email=${email}`);
   }
 
   @Post('refresh')
