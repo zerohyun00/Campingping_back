0ccd6c3725232ffcd176b9245f82429266966ae9
[Node1] 백기준 <rlwns1580@naver.com>
2025-01-27 22:20:50 +0900

    ref: 인터셉터 수정

--- src/common/interceptor/transformation.intersepter.ts
+++ src/common/interceptor/transformation.intersepter.ts
@@ -7,15 +7,18 @@ export class TransformInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     
-    // Prometheus와 Grafana 엔드포인트는 인터셉터를 거치지 않도록 설정
-    // if (request.url.includes('/api/metrics') || request.url.includes('/api/grafana')) {
-    //   return next.handle(); // 인터셉터를 적용하지 않고 바로 처리
-    // }
+    //Prometheus와 Grafana 엔드포인트는 인터셉터를 거치지 않도록 설정
+    if (request.url.includes('/api/metrics') || request.url.includes('/api/grafana')) {
+      return next.handle(); // 인터셉터를 적용하지 않고 바로 처리
+    }
 
     // 다른 엔드포인트는 정상적으로 인터셉터가 적용됨
     return next.handle().pipe(
       tap((data) => {
-        // 응답 변형 처리 (예: JSON 응답 형식 변경 등)
+        return {
+          statusCode: context.switchToHttp().getResponse().statusCode,
+          data
+        };
       }),
     );
   }
