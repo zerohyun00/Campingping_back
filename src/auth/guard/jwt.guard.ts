101f0ddb1fada722fd8899324b6f6932577b9de9
[Node1] 백기준 <rlwns1580@naver.com>
2025-01-31 19:46:29 +0900

    ref: 캠핑 가드 수정 및 에러로그 추가:

--- src/auth/guard/jwt.guard.ts
+++ src/auth/guard/jwt.guard.ts
@@ -23,16 +23,16 @@ export class JwtAuthGuard implements CanActivate {
         token = authHeader.split(' ')[1]; // "Bearer " 부분 제거
       }
     }
-    // if(!token){
-    //   return false;
-    // }
+    if(!token){
+      return false;
+    }
     
     try {
       const user = this.jwtService.verify(token); // 토큰 검증
       request.user = user; // 인증된 사용자 정보 설정
       return true;
     } catch (error) {
-      console.error('JWT 검증 오류: ', error.message); // 오류 로그 추가
+      console.error('JWT 검증 오류: authGuard', error.message); // 오류 로그 추가
       return false;
     }
   }
--- src/camping/guard/camping.jwt.guard.ts
+++ src/camping/guard/camping.jwt.guard.ts
@@ -16,14 +16,22 @@ export class CampingJwtAuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest<CampingUserRequest>(); // 타입 지정
     let token = request.cookies['accessToken']; // 쿠키에서 토큰 가져오기
 
-    if(!token) {
-        return true;
+    if (!token) {
+      const authHeader = request.headers['authorization'];
+      if (authHeader && authHeader.startsWith('Bearer ')) {
+        token = authHeader.split(' ')[1]; 
+      }
     }
+
+    if (!token) {
+      return true;
+    }
+
     try {
       const user = this.jwtService.verify(token); // 토큰 검증
       request.user = user; // 인증된 사용자 정보
     } catch (error) {
-      console.error('JWT 검증 오류: ', error.message); // 오류 로그 추가
+      console.error('JWT 검증 오류: campingGuard', error.message); // 오류 로그 추가
     }
     return true; // 요청을 계속 진행
   }
